61dcb12e218064038a4ac36d8d7deafb8f0c2508
Behzad Khosravifar <Behzad.khosravifar@gmail.com>
2017-02-03 16:39:21 +0330

    first init

---
+++ src/IXL/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="IXL.MvcApplication" Language="C#" %>
---
+++ src/IXL/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(IXL.Startup))]
+namespace IXL
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
