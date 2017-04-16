9eb9962d535e0d5be41fb7c2bc7222bd7128b3d5
Behzad Khosravifar <Behzad.khosravifar@gmail.com>
2017-04-16 16:11:51 +0430

    fix SSL problem by convert http address to https

--- src/IXL/Properties/AssemblyInfo.cs
+++ src/IXL/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.90")]
-[assembly: AssemblyFileVersion("1.0.90")]
+[assembly: AssemblyVersion("1.0.92")]
+[assembly: AssemblyFileVersion("1.0.92")]
